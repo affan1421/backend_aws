826de44d131c18832607aabb81c06e6254d4a0f3
affan <affan.cumin@gmail.com>
2023-12-18 18:10:28 +0530

    backend 11

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-#Stopping existing node servers
-echo "Stopping any existing node servers"
-sudo pkill node
+
+# Stop existing Node.js servers
+NODE_PROCESSES=$(pgrep -f "node")
+
+if [ -n "$NODE_PROCESSES" ]; then
+  echo "Stopping existing Node.js servers: $NODE_PROCESSES"
+  kill -TERM $NODE_PROCESSES
+else
+  echo "No existing Node.js servers found"
+fi

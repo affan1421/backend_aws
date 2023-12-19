98535e48a1be6f8500f7e16f727494441b901cb0
affan <affan.cumin@gmail.com>
2023-12-19 18:13:28 +0530

    backend 34

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,7 +6,6 @@ cd /home/ubuntu/express-app
 #_Delete_Old_PM2_Service
 #sudo pm2 delete Frontend
 #sudo pm2 start server.js --name Frontend
-pm2 delete Backend
-pm2 start index.js --name Backend
+pm2 start index.js 
 pm2 save
 pm2 startup

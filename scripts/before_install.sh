9bb7dea684b03fb6fd1ae24151fc65868abafdbb
affan <affan.cumin@gmail.com>
2023-12-19 16:05:48 +0530

    Revert "backend 17"
    This reverts commit 5e9ef9f13e6aef16edaa07354780d27154319fff.

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,17 @@
 # Deleted the old project before new deploy
 #!/bin/bash
-set -e
-​
 redis-cli flushall
 
 sudo rm -rf /home/ubuntu/express-app/*
+#_Change_Working_Directory
+cd /home/ubuntu/express-app
+
+#_Update_&_Set_Node_Version
+curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+
+#_Download_Node_&NPM
+sudo apt-get install -y nodejs
+
+#_Download_PM2
+npm install pm2@latest -g
+

4b39708549312b507b9016a880ab5d59505b95b1
affan <affan.cumin@gmail.com>
2023-12-20 13:26:42 +0530

    backend44

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,12 +1,12 @@
-    #!/bin/bash
-    set -e
-    #_Change_Working_Directory
-    cd /home/ubuntu/express-app
+#!/bin/bash
+set -e
+#_Change_Working_Directory
+cd /home/ubuntu/express-app
 
-    #_Remove_Unused_Code
-    rm -rf node_modules
+#_Remove_Unused_Code
+rm -rf node_modules
 
-    #Install_node_modules_&_Make_React_Build
-    npm  install --force
+#Install_node_modules_&_Make_React_Build
+npm  install --force
 
 

d0b22286459f3a59fa7e22bf03f2bd5d353a95f3
affan <affan.cumin@gmail.com>
2023-12-20 17:00:05 +0530

    update

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,6 +4,8 @@
 cd /home/ubuntu/express-app
 
 sudo rm -rf /home/ubuntu/express-app/*
+rm -rf node_modules package-lock.json
+
 
 # #_Update_&_Set_Node_Version
 # curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
